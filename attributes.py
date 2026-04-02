3e459c49176fd598cee30bab07d84362d593f776
hexi <22331676+kwijik@users.noreply.github.com>
2026-04-02 22:35:05 +0300

    attributes file

---
+++ attributes.py
@@ -0,0 +1,25 @@
+COMMON_ATTRIBUTES = {
+    "color": [
+        "black", "white", "red", "blue", "green", "yellow", "pink", 
+        "gray", "brown", "beige", "purple", "orange", "multicolor"
+    ],
+    "pattern": [
+        "solid no pattern", "striped", "floral", "checkered/plaid", 
+        "polka dot", "graphic print", "animal print"
+    ]
+}
+
+CLASS_SPECIFIC_ATTRIBUTES = {
+    "short sleeve top": {
+        "fit": ["fitted", "oversized", "cropped", "regular fit"],
+        "neckline": ["crew neck", "v-neck", "scoop neck", "polo collar"],
+    },
+    "skirt": {
+        "length": ["mini", "midi", "maxi"],
+        "silhouette": ["pencil", "pleated", "flared", "wrap", "denim skirt"],
+    },
+    "trousers": {
+        "fit": ["skinny", "straight", "wide leg", "cargo", "sweatpants", "jeans"],
+    }
+    # to be continued...
+}
